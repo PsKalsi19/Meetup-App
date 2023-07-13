9bc056bc1d777102e0cf0073e5ef02092b4cd641
Prabhjot Kalsi <105273248+PsKalsi19@users.noreply.github.com>
2023-07-13 19:45:30 +0530

    initial setup

---
+++ src/context/DataProvider.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react";
+
+export const DataContext = createContext();
+
+const DataProvider = ({ children }) => {
+  return <DataContext.Provider value={{
+    
+  }}>{children}</DataContext.Provider>;
+};
+
+export default DataProvider;
